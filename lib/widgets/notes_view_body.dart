a29333378405c593b1e92c1e6f1af03104480e2a
ismail-24 <ismailtarek888@gmail.com>
2023-11-17 15:21:11 +0200

    get font & create custom note item

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_AppBar.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: const Column(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
         children: [
           SizedBox(
             height: 40,
           ),
           CustomAppBar(),
+          NoteItem()
         ],
       ),
     );
