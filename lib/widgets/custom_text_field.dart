ede4e5e04f2900d3219c5bca937a4521d35f04e8
ismail-24 <ismailtarek888@gmail.com>
2023-11-25 10:33:52 +0200

    edit note done

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  CustomTextField({super.key, required this.hint, this.maxLines, this.onSaved});
+  CustomTextField({
+    super.key,
+    required this.hint,
+    this.maxLines,
+    this.onSaved,
+    this.onChanged,
+  });
+
   final String hint;
   final int? maxLines;
 
   final void Function(String?)? onSaved;
+  final Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       onSaved: onSaved,
       validator: (value) {
         if (value?.isEmpty ?? true) {
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_AppBar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
-class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({
-    super.key,
-  });
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
 
+  final NoteModel note;
+
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, content;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,18 +25,36 @@ class EditNoteViewBody extends StatelessWidget {
           const SizedBox(
             height: 50,
           ),
-          const CustomAppBar(
-            title: 'Edit',
+          CustomAppBar(
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subTitle = content ?? widget.note.subTitle;
+              widget.note.save();
+
+              BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+              Navigator.pop(context);
+            },
+            title: 'Edit Note',
             icon: Icons.check,
           ),
           const SizedBox(
             height: 32,
           ),
-          CustomTextField(hint: 'Title', maxLines: 2),
+          CustomTextField(
+              onChanged: (value) {
+                title = value;
+              },
+              hint: widget.note.title,
+              maxLines: 2),
           const SizedBox(
             height: 16,
           ),
-          CustomTextField(hint: 'Content', maxLines: 5),
+          CustomTextField(
+              onChanged: (value) {
+                content = value;
+              },
+              hint: widget.note.subTitle,
+              maxLines: 5),
         ],
       ),
     );
